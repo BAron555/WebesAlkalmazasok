dd17768707bf2ddd0b21ed2ba62bc2aa9758080b
BAron555 <terrangamer97@gmail.com>
2021-12-22 22:26:33 +0100

    DependencyTest

---
+++ entity/src/main/java/uni/miskolc/entity/Dependency.java
@@ -0,0 +1,9 @@
+package uni.miskolc.entity;
+
+public interface Dependency {
+    void helpNoParameterNoReturnValue();
+    int helpNoParameterReturnValue();
+    String importantString(String string);
+    void helpWithParameterNoReturnValue(int a);
+   
+}
---
+++ entity/src/main/java/uni/miskolc/entity/Service.java
@@ -0,0 +1,5 @@
+package uni.miskolc.entity;
+
+public interface Service {
+    void calculate();
+}
